5e7b1ca5ed7a8d8259f6724834d2100003627731
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-03-05 13:58:31 +0000

    Initial commit

---
+++ client/webpack/dev.config.js
@@ -0,0 +1,17 @@
+const { merge } = require("webpack-merge");
+
+const common = require("./common.config");
+
+module.exports = merge(common, {
+	devtool: "inline-source-map",
+	devServer: {
+		historyApiFallback: {
+			disableDotRule: true,
+		},
+		port: 3000,
+		proxy: {
+			"/api": "http://localhost:3100",
+		},
+	},
+	mode: "development",
+});
