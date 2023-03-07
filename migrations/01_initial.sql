d5ea308f481a4ce11dc27ec4d4d0e15b4dede679
fatih-celebi <celebifatih.c1@gmail.com>
2023-03-07 20:27:40 +0000

    Update 01_initial.sql

--- migrations/01_initial.sql
+++ migrations/01_initial.sql
@@ -2,13 +2,13 @@ CREATE TABLE users (
   id       SERIAL PRIMARY KEY,
   name     VARCHAR(255),
   region   VARCHAR(255),
-  role     VARCHAR(20),
+  role     VARCHAR(20)
 );
 
 CREATE TABLE sessions (
   id          SERIAL PRIMARY KEY,
   name        VARCHAR(30),
-  time        DATETIME,
+  time        TIMESTAMP,
   cohort_id   INT REFERENCES cohorts(id),
   meeting_url VARCHAR(2048)
 );
@@ -23,6 +23,6 @@ CREATE TABLE attendence (
   id           SERIAL PRIMARY KEY,
   user_id      INT REFERENCES users(id),
   session_id   INT REFERENCES sessions(id),
-  clockin_time DATETIME,
+  clockin_time TIMESTAMP,
   notes        VARCHAR(1024)
 );
