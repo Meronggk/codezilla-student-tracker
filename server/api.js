f6fa457027f6672b75b42d0b61f3177f95afe133
ShimenAfshar <afsharshimen@gmail.com>
2023-03-09 21:13:59 +0000

    fix-lint

--- server/api.js
+++ server/api.js
@@ -4,7 +4,6 @@ import logger from "./utils/logger";
 
 import db from "./db";
 
-
 const router = Router();
 
 router.get("/", (_, res) => {
@@ -13,20 +12,22 @@ router.get("/", (_, res) => {
 });
 
 router.get("/getZoomMeeting/:id", function (req, res) {
-
 	const sessionid = parseInt(req.params.id);
 	let userid;
 	let data = {};
-	db.query("SELECT * FROM sessions WHERE id = $1", [sessionid], (error, result) => {
-		if (error) {
-			throw error;
+	db.query(
+		"SELECT * FROM sessions WHERE id = $1",
+		[sessionid],
+		(error, result) => {
+			if (error) {
+				throw error;
+			}
+			data.link = result[0].meeting_url;
+			data.name = result[0].name;
+			data.time = result[0].time;
+			userid = result[0].userid;
 		}
-		data.link = result[0].meeting_url;
-		data.name = result[0].name;
-		data.time = result[0].time;
-		userid = result[0].userid;
-
-	});
+	);
 
 	//insert student req date in attendance//
 	const clockintime = new Date();
