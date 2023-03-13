68a1e4368c6b748c7d7b5c8d9524106681b38e4c
fatih-celebi <celebifatih.c1@gmail.com>
2023-03-13 23:55:36 +0000

    Create get endpoint to get user details

--- server/api.js
+++ server/api.js
@@ -117,4 +117,26 @@ router.post("/logout", (req, res) => {
 	res.sendStatus(204); // No Content
 });
 
+// Endpoint for getting user details
+router.get("/users/:id", async (req, res) => {
+	const userId = req.params.id;
+	const query = "SELECT * FROM users WHERE id = $1";
+	try {
+		const result = await db.query(query, [userId]);
+		if (result.rows.length === 0) {
+			res.status(404).json({ message: "User not found" });
+		} else {
+			const user = result.rows[0];
+			res.json({
+				id: user.id,
+				name: user.name,
+				email: user.email,
+			});
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Internal server error" });
+	}
+});
+
 export default router;
