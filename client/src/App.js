5fa474f64ac117a2b59952c1a10cfca7cc11378a
fatih-celebi <celebifatih.c1@gmail.com>
2023-03-16 17:57:30 +0000

    Commented Out Navbar and Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import GitHubCalllBack from "./components/GitHubCallBack";
 // import GitHubLogin from "./components/GitHubLogin";
-import Login from "./components/Login";
+// import Login from "./components/Login";
 import About from "./pages/About";
 import Home from "./pages/Home";
 
@@ -9,13 +9,12 @@ import Home from "./pages/Home";
 function App() {
 	return (
 		<div>
-			<NavBar />
+			{/**<NavBar /> **/}
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about/this/site" element={<About />} />
 				{/* <Route path="/login" element={<GitHubLogin></GitHubLogin>} /> */}
 				<Route path="/callback" element={<GitHubCalllBack />} />
-
 			</Routes>
 		</div>
 	);
