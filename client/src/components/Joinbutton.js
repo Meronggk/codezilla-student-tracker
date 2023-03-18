1d241f0e9d02dfa6fe10cba021cbac70967dfe80
ShimenAfshar <afsharshimen@gmail.com>
2023-03-18 15:30:46 +0000

    set target time

--- client/src/components/Joinbutton.js
+++ client/src/components/Joinbutton.js
@@ -1,22 +1,36 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 function JoinButton() {
-
-function handleClick(){
+	const [isClickable, setIsClickable] = useState(false);
+	useEffect(() => {
+		const currentTime = new Date();
+		const targetTime = new Date();
+		targetTime.setHours(10, 0, 0); // set target time to 10:00 AM
+		const timeDifference = targetTime.getTime() - currentTime.getTime();
+		const isAfterTargetTime = timeDifference < 0;
+		const isBefore10Mins = timeDifference > -10 * 60 * 1000; // 10 mins before target time
+		if (isAfterTargetTime || !isBefore10Mins) {
+			setIsClickable(false);
+			return;
+		}
+		const intervalId = setInterval(() => {
+			setIsClickable(true);
+		}, timeDifference);
+		return () => clearInterval(intervalId);
+	}, []);
+	function handleClick() {
+		console.log('test')
 		fetch("/api/getZoomMeeting/1")
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data.meeting_url);
 			});
-}
-
+	}
 	return (
-		<button onClick={handleClick}>
+		<button onClick={handleClick} disabled={!isClickable}>
 			join button
 		</button>
 	);
 }
-
 export default JoinButton;
 
-i
+
