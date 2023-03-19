57a7839daf1ef311d93487361b484c4b15a199d8
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-03-19 21:01:02 +0000

    fixed an error on navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,9 @@ import Login from "./components/Login";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Homepage from "./components/Homepage/Homepage";
-// import NavBar from "./components/Homepage/NavBar";
+import NavBar from "./components/Homepage/NavBar";
 import Form from "./components/Form";
 import SessionDash from "./components/SessionDash";
-// import AddClass from "./components/AddClass";
 import NewSession from "./components/NewSession";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./pages/Profile";
@@ -16,7 +15,7 @@ import RegisterUser from "./components/RegisterUser";
 function App() {
 	return (
 		<div>
-			{/* <NavBar />  */}
+			<NavBar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about/this/site" element={<About />} />
