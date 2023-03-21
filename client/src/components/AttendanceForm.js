9ab41b008af624655fcb0d28ae255eed672c924d
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-03-21 21:34:05 +0000

    added Css

--- client/src/components/AttendanceForm.js
+++ client/src/components/AttendanceForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 // eslint-disable-next-line no-unused-vars
 import { Form } from "react-router-dom";
+import "./AttendanceForm.css";
 
 const fakeUsers = [];
 
