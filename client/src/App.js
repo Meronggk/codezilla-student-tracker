a73e3b647eb569bc5cd01e40f20af8e1f81fc308
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-03-22 10:42:16 +0000

    made change hompage

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import GitHubCalllBack from "./components/GitHubCallBack";
 import GitHubLogin from "./components/GitHubLogin";
 import Login from "./components/Login";
-import About from "./pages/About";
-import Home from "./pages/Home";
 import Homepage from "./components/Homepage/Homepage";
 import NavBar from "./components/Homepage/NavBar";
 import AttendenceForm from "./components/AttendenceForm";
 import SessionDash from "./components/SessionDash";
 import NewSession from "./components/NewSession";
 import LogoutButton from "./components/LogoutButton";
-import Profile from "./pages/Profile";
+import Profile from "./components/Profile";
 import RegisterUser from "./components/RegisterUser";
 
 function App() {
@@ -18,12 +16,10 @@ function App() {
 		<div>
 			<NavBar />
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about/this/site" element={<About />} />
+			<Route path="/" element={<Homepage />} />
 				<Route path="/login" element={<GitHubLogin></GitHubLogin>} />
 				<Route path="/callback" element={<GitHubCalllBack />} />
 				<Route path="/signin" element={<Login />} />
-				<Route path="/homepage" element={<Homepage />} />
 				<Route path="/attendenceForm" element={<AttendenceForm />} />
 				<Route path="/dashboard" element={<SessionDash />} />
 
