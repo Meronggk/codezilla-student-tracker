3fc529959128e4262a62e6d8d7899cb3083b8f2b
fatih-celebi <celebifatih.c1@gmail.com>
2023-03-23 19:46:41 +0000

    Add more data for testing

--- server/database.js
+++ server/database.js
@@ -34,42 +34,112 @@ const db = new Pool({
 });
 
 const users = [
-	{ name: "John", region: "North West", role: "Trainee" },
-	{ name: "Denise", region: "London", role: "Volunteer" },
-	{ name: "Josh", region: "West Midlands", role: "Manager" },
-	{ name: "Kyle", region: "Scotland", role: "Trainee" },
-	{ name: "Stephen", region: "Cape Town", role: "Volunteer" },
+	{ name: "John Taylor", region: "North West", role: "Trainee" },
+	{ name: "Denise Rivers", region: "London", role: "Volunteer" },
+	{ name: "Josh Martin", region: "West Midlands", role: "Volunteer" },
+	{ name: "Kyle Smith", region: "Scotland", role: "Trainee" },
+	{
+		name: "Fatih Celebi",
+		region: "North West",
+		role: "Trainee",
+		github_user_id: 97455723,
+		github_username: "fatih-celebi",
+	},
+	{
+		name: "Shimen Afshar",
+		region: "North West",
+		role: "Trainee",
+		github_user_id: 106038638,
+		github_username: "ShimenAfshar",
+	},
+	{
+		name: "Mohammad Nafar",
+		region: "North West",
+		role: "Trainee",
+		github_user_id: 104798944,
+		github_username: "Mohammadrezanafar",
+	},
+	{
+		name: "Meron Gebremichael",
+		region: "North West",
+		role: "Trainee",
+		github_user_id: 99277162,
+		github_username: "Meronggk",
+	},
+	{ name: "Emily Wyatt", region: "North West", role: "Volunteer" },
 ];
 
 const sessions = [
 	{
 		name: "JavaScript-1 Week-1",
-		time: "2023-03-11 10:00:00",
-		cohort_name: "NW-2",
+		time: "2023-02-25 10:00:00",
+		cohort_name: "NW-5",
 		meeting_url: "https://www.youtube.com/watch?v=tAGnKpE4NCI",
 	},
 	{
 		name: "JavaScript-1 Week-2",
-		time: "2023-03-12 14:00:00",
-		cohort_name: "NW-3",
+		time: "2023-03-04 10:00:00",
+		cohort_name: "NW-5",
 		meeting_url: "https://www.youtube.com/watch?v=JGwWNGJdvx8",
 	},
 	{
 		name: "JavaScript-1 Week-3",
-		time: "2023-03-13 09:00:00",
-		cohort_name: "NW-4",
+		time: "2023-03-11 10:00:00",
+		cohort_name: "NW-5",
 		meeting_url: "https://www.youtube.com/watch?v=nYh-n7EOtMA",
 	},
 	{
-		name: "JavaScript-2 Week-1",
-		time: "2023-03-14 11:00:00",
+		name: "JavaScript-1 Week-4",
+		time: "2023-03-12 10:00:00",
 		cohort_name: "NW-5",
 		meeting_url: "https://www.youtube.com/watch?v=GzU8KqOY8YA",
 	},
 	{
+		name: "JavaScript-2 Week-1",
+		time: "2023-03-25 10:00:00",
+		cohort_name: "NW-5",
+		meeting_url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+	},
+	{
+		name: "JavaScript-2 Week-2",
+		time: "2023-04-01 10:00:00",
+		cohort_name: "NW-5",
+		meeting_url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+	},
+	{
+		name: "JavaScript-2 Week-3",
+		time: "2023-04-08 10:00:00",
+		cohort_name: "NW-5",
+		meeting_url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+	},
+	{
+		name: "JavaScript-2 Week-4",
+		time: "2023-04-15 10:00:00",
+		cohort_name: "NW-5",
+		meeting_url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+	},
+	{
+		name: "JavaScript-3 Week-1",
+		time: "2023-04-22 10:00:00",
+		cohort_name: "NW-5",
+		meeting_url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+	},
+	{
+		name: "JavaScript-3 Week-2",
+		time: "2023-04-29 10:00:00",
+		cohort_name: "NW-5",
+		meeting_url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+	},
+	{
 		name: "JavaScript-3 Week-3",
-		time: "2023-03-15 13:00:00",
-		cohort_name: "NW-2",
+		time: "2023-05-06 10:00:00",
+		cohort_name: "NW-5",
+		meeting_url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+	},
+	{
+		name: "JavaScript-3 Week-4",
+		time: "2023-05-13 10:00:00",
+		cohort_name: "NW-5",
 		meeting_url: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
 	},
 ];
@@ -84,33 +154,33 @@ const cohorts = [
 
 const attendence = [
 	{
-		user_name: "John",
-		session_name: "JavaScript-1 Week-1",
-		clockin_time: "2023-03-15 10:00:52",
+		user_name: "John Taylor",
+		session_name: "JavaScript-1 Week-4",
+		clockin_time: "2023-03-12 10:00:52",
 		notes: "Student arrived on time",
 	},
 	{
-		user_name: "Denise",
-		session_name: "JavaScript-1 Week-1",
-		clockin_time: "2023-03-15 10:00:52",
+		user_name: "Denise Rivers",
+		session_name: "JavaScript-1 Week-4",
+		clockin_time: "2023-03-12 10:00:52",
 		notes: "Student arrived on time",
 	},
 	{
-		user_name: "Josh",
-		session_name: "JavaScript-1 Week-1",
-		clockin_time: "2023-03-15 10:00:52",
+		user_name: "Josh Martin",
+		session_name: "JavaScript-1 Week-4",
+		clockin_time: "2023-03-12 10:00:52",
 		notes: "Student arrived on time",
 	},
 	{
-		user_name: "Kyle",
-		session_name: "JavaScript-1 Week-1",
-		clockin_time: "2023-03-15 10:16:52",
+		user_name: "Kyle Smith",
+		session_name: "JavaScript-1 Week-4",
+		clockin_time: "2023-03-12 10:16:52",
 		notes: "Student arrived late due to traffic",
 	},
 	{
-		user_name: "Stephen",
-		session_name: "JavaScript-1 Week-1",
-		clockin_time: "2023-03-15 10:28:26",
+		user_name: "Fatih Celebi",
+		session_name: "JavaScript-1 Week-4",
+		clockin_time: "2023-03-12 10:28:26",
 		notes: "Student arrived late due to train strike",
 	},
 ];
@@ -146,8 +216,14 @@ const runQuery = async (queries) => {
 
 users.forEach((user) => {
 	const query = {
-		text: "INSERT INTO users(name, region, role) VALUES($1, $2, $3)",
-		values: [user.name, user.region, user.role],
+		text: "INSERT INTO users(name, region, role, github_user_id, github_username) VALUES($1, $2, $3, $4, $5)",
+		values: [
+			user.name,
+			user.region,
+			user.role,
+			user.github_user_id,
+			user.github_username,
+		],
 	};
 	allQueries.push(query);
 });
