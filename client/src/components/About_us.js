8fd2366c37eddc8850bceb83d43ab373c44ecf5e
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-03-24 19:31:15 +0000

    lint fix

--- client/src/components/About_us.js
+++ client/src/components/About_us.js
@@ -30,84 +30,108 @@ const teamMembers = [
 		linkedin: "https://www.linkedin.com/in/mohammadreza-nafar-669507239/",
 		image: require("./Homepage/assets/images/Mohammed.png"),
 	},
-    {
-    id: 5,
-    name: "Katie",
-    position: "Product Manager",
-    image: require("./Homepage/assets/images/Katie.png"),
-    },
-    {
-        id: 6,
-        name: "Raj",
-        position: "TA",
-        image: require("./Homepage/assets/images/Raj.png"),
-    },
-    // {
-    //     id: 7,
-    //     name: "Emily",
-    //     position: "TA",
-    //     // image: require(Emily),
-    // },
-
+	{
+		id: 5,
+		name: "Katie",
+		position: "Product Manager",
+		image: require("./Homepage/assets/images/Katie.png"),
+	},
+	{
+		id: 6,
+		name: "Raj",
+		position: "TA",
+		image: require("./Homepage/assets/images/Raj.png"),
+	},
+	// {
+	//     id: 7,
+	//     name: "Emily",
+	//     position: "TA",
+	//     // image: require(Emily),
+	// },
 ];
 
-function About_us () {
-  const [showMembers, setShowMembers] = useState(false);
-  const [showProject, setShowProject] = useState(false);
+function About_us() {
+	const [showMembers, setShowMembers] = useState(false);
+	const [showProject, setShowProject] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setShowMembers(true);
-    }, 2000);
-  }, []);
+	useEffect(() => {
+		setTimeout(() => {
+			setShowMembers(true);
+		}, 2000);
+	}, []);
 
-  useEffect(() => {
-    if (showMembers) {
-      setTimeout(() => {
-        setShowProject(true);
-      }, 2000);
-    }
-  }, [showMembers]);
+	useEffect(() => {
+		if (showMembers) {
+			setTimeout(() => {
+				setShowProject(true);
+			}, 2000);
+		}
+	}, [showMembers]);
 
-  return (
-    <div style={{ backgroundColor: "darkgrey", minHeight: "100vh", color: "black" }} className="about_cont">
-      <div style={{ padding: "20px", color: "black" }}>
-        <h1 style={{ textAlign: "center", color: "black" }}>Meet the team</h1>
-        <p style={{ textAlign: "center", fontSize: "25px", color: "black" }}>
- Codezilla Group
-        </p>
-      </div>
+	return (
+		<div
+			style={{
+				backgroundColor: "darkgrey",
+				minHeight: "100vh",
+				color: "black",
+			}}
+			className="about_cont"
+		>
+			<div style={{ padding: "20px", color: "black" }}>
+				<h1 style={{ textAlign: "center", color: "black" }}>Meet the team</h1>
+				<p style={{ textAlign: "center", fontSize: "25px", color: "black" }}>
+					Codezilla Group
+				</p>
+			</div>
 
-      {showMembers && (
-        <div style={{ display: "flex", justifyContent: "center", color: "black" }}>
-          {teamMembers.map((member) => (
-            <div key={member.id} style={{ textAlign: "center", margin: "20px", color: "black" }}>
-              <img
-                src={member.image.default}
-                alt={member.name}
-                style={{ width: "150px", height: "150px", borderRadius: "10%", color: "black" }}
-              />
-              <h3>
-                <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
-                  {member.name}
-                </a>
-              </h3>
-              <p style={{ color: "black" }}>{member.position}</p>
-            </div>
-          ))}
-        </div>
-      )}
+			{showMembers && (
+				<div
+					style={{ display: "flex", justifyContent: "center", color: "black" }}
+				>
+					{teamMembers.map((member) => (
+						<div
+							key={member.id}
+							style={{ textAlign: "center", margin: "20px", color: "black" }}
+						>
+							<img
+								src={member.image.default}
+								alt={member.name}
+								style={{
+									width: "150px",
+									height: "150px",
+									borderRadius: "10%",
+									color: "black",
+								}}
+							/>
+							<h3>
+								<a
+									href={member.linkedin}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									{member.name}
+								</a>
+							</h3>
+							<p style={{ color: "black" }}>{member.position}</p>
+						</div>
+					))}
+				</div>
+			)}
 
-      {showProject && (
-        <>
-          <h2 style={{ textAlign: "center", marginTop: "100px", color: "black" }}>OUR PROJECT</h2>
-          <p style={{ textAlign: "center", color: "black", fontSize: "28px" }}>
- Class Register for CYF
-          </p>
-        </>
-      )}
-    </div>
-  );
+			{showProject && (
+				<>
+					<h2
+						style={{ textAlign: "center", marginTop: "100px", color: "black" }}
+					>
+						OUR PROJECT
+					</h2>
+					<p style={{ textAlign: "center", color: "black", fontSize: "28px" }}>
+						Class Register for CYF
+					</p>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default About_us;
