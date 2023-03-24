a89d83fbd10db4329f4ff19ccf7472d0f393cfb0
ShimenAfshar <afsharshimen@gmail.com>
2023-03-24 15:55:11 +0000

    url meeting

--- client/src/components/Joinbutton.js
+++ client/src/components/Joinbutton.js
@@ -1,16 +1,69 @@
+// import React, { useState, useEffect } from "react";
+// import { isWithinInterval, add } from "date-fns";
+// // import Button from "react-bootstrap/Button";
+// import "./Joinbutton.css";
+
+// function JoinButton() {
+// 	const [isClickable, setIsClickable] = useState(false);
+// 	const [jointime, setJoinTime] = useState("");
+// 	const [meetingurl, setMeetingUrl] = useState("");
+// 	useEffect(() => {
+// 		const currentTime = new Date();
+// 		const targetTime = new Date();
+// 		targetTime.setHours(14, 50, 0); // set target time to 10:00 AM
+// 		const isAfterClass1Hour = add(targetTime, { hours: 1, minutes: 10 });
+// 		setIsClickable(
+// 			isWithinInterval(currentTime, {
+// 				start: targetTime,
+// 				end: isAfterClass1Hour,
+// 			})
+// 		);
+// 		console.log(
+// 			isWithinInterval(currentTime, {
+// 				start: targetTime,
+// 				end: isAfterClass1Hour,
+// 			})
+// 		);
+// 	}, []);
+
+// 	async function handleClick() {
+// 		const requestOptions = {
+// 			method: "POST",
+// 			headers: { "Content-Type": "application/json" },
+// 			body: JSON.stringify({ id: 1 }),
+// 		};
+
+// 		const response = await fetch("http://localhost:3100/api/joinSession", requestOptions);
+// 		const data = await response.json();
+// 		setMeetingUrl(data.meetingurl);
+// 		setJoinTime(data.jointime);
+
+// 	}
+// 	// if (isClickable) {
+// 	return (<><button onClick={handleClick}> Join button </button><h3><a href={meetingurl}> {meetingurl} </a></h3><h3>{jointime}</h3><h2>{meetingurl}</h2></>
+// 		);
+// 	// } else {
+// 	// 	return <div> no class presently</div>;
+// 	// }
+// }
+// export default JoinButton;
+
 import React, { useState, useEffect } from "react";
 import { isWithinInterval, add } from "date-fns";
 // import Button from "react-bootstrap/Button";
 import "./Joinbutton.css";
+import { useNavigate } from "react-router";
+// import { Link } from "react-router-dom";
 
 function JoinButton() {
+	const navigate = useNavigate;
 	const [isClickable, setIsClickable] = useState(false);
-	const [jointime, setJoinTime] = useState("there is no class at this time");
-	const [meetingurl, setMeetingUrl] = useState("there is no link");
+	const [jointime, setJoinTime] = useState("");
+	const [meetingurl, setMeetingUrl] = useState("");
 	useEffect(() => {
 		const currentTime = new Date();
 		const targetTime = new Date();
-		targetTime.setHours(9, 50, 0); // set target time to 10:00 AM
+		targetTime.setHours(14, 50, 0); // set target time to 10:00 AM
 		const isAfterClass1Hour = add(targetTime, { hours: 1, minutes: 10 });
 		setIsClickable(
 			isWithinInterval(currentTime, {
@@ -33,15 +86,20 @@ function JoinButton() {
 			body: JSON.stringify({ id: 1 }),
 		};
 
-		const response = await fetch("http://localhost:3100/api/joinSession", requestOptions);
+		const response = await fetch("/api/joinSession", requestOptions);
 		const data = await response.json();
 		setMeetingUrl(data.meetingurl);
 		setJoinTime(data.jointime);
+		navigate("/joinSession");
 
 	}
 	// if (isClickable) {
-	return (<><button onClick={handleClick}> Join button </button><h3><a href={meetingurl}> {meetingurl} </a></h3><h3>{jointime}</h3><h2>{meetingurl}</h2></>
-		);
+	return (<><button onClick={handleClick}> Join button </button><h3>
+		<div><a href={meetingurl}>{meetingurl}</a></div>
+		</h3>
+		<h3>{jointime}</h3>
+		<h2>{meetingurl}</h2></>
+	);
 	// } else {
 	// 	return <div> no class presently</div>;
 	// }
